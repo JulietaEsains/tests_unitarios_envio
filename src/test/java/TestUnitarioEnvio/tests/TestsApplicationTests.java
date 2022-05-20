da0ff036e19b7b4852c7c695259c38c02ad09d4e
JulietaEsains <jesains@gmail.com>
2022-05-20 16:44:40 -0300

    primer commit

---
+++ src/test/java/TestUnitarioEnvio/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package TestUnitarioEnvio.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
