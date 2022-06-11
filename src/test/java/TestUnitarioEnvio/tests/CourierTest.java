8c0367dd87a1ec4082aadc4d0f8735345dad5ae9
Lucas Lopez <lucas.lopez@alumnos.frm.utn.edu.ar>
2022-06-11 19:21:10 -0300

    Comentario añadido por Lucas

--- src/test/java/TestUnitarioEnvio/tests/CourierTest.java
+++ src/test/java/TestUnitarioEnvio/tests/CourierTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+//Comentario de lucas
 @SpringBootTest
 public class CourierTest {
     // El courier debe tener un nombre asignado. Es decir si accedemos a la propiedad nombreCourier de un courier, esta debe ser distinta de nula.
